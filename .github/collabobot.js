a574bec638904ce8e5a98c9f64ccac00696101ad
Frank Zhao <syzhao1988@126.com>
2019-04-14 19:50:27 +0800

    Update collabobot.js

--- .github/collabobot.js
+++ .github/collabobot.js
@@ -1,4 +1,14 @@
 module.exports = {
+  labelSetupComponent: {
+    enable: true,
+    customLabels: [
+      {
+        name: "customLabel",
+        description: "test label",
+        color: "0366d6"
+      }
+    ]
+  },
   welcomeInstallComponent: {
     enable: true,
     title: "What? I just want to try.",
